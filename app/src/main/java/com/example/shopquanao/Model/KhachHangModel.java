09b931a9552cddc288fa8a1d0ea749235ee50989
YoungBirds134 <huynguyen2913@gmail.com>
2021-05-25 23:51:16 +0700

    create proc lay_KhachHang as Select * from KhachHang exec lay_KhachHang

--- app/src/main/java/com/example/shopquanao/Model/KhachHangModel.java
+++ app/src/main/java/com/example/shopquanao/Model/KhachHangModel.java
@@ -1,14 +1,10 @@
 package com.example.shopquanao.Model;
-import android.content.Context;
-import android.util.Log;
-import android.widget.Toast;
 
 import com.example.shopquanao.DB.ConnectionDB;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 
 public class KhachHangModel {
@@ -20,7 +16,30 @@ public class KhachHangModel {
 
 
 
+    public ArrayList<KhachHang> lay_KhachHang(){
+        ArrayList<KhachHang> arrayList = new ArrayList<>();
+        try {
+            if (z==null)
+            {
+                z="Connection DB Fail!";
+            }else
+            {
+                PreparedStatement statement = con.prepareStatement("exec lay_KhachHang");
 
+                rs = statement.executeQuery();
+
+                while (rs.next()) {
+                    arrayList.add(new KhachHang(rs.getInt("MaKH"),rs.getString("TenKH"),rs.getString("DiaChi"),rs.getString("SDT"),rs.getString("MatKhau")));// Đọc dữ liệu từ ResultSet
+                }
+
+                con.close();// Đóng kết nối
+            }
+        }catch (Exception e)
+        {
+            z = "Exceptions";
+        }
+        return  arrayList;
+    }
 
 
 }
